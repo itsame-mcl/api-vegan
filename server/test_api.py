363a35a2a0d6583fbf14f07b32a2c375cc803769
Maxence Lagalle <contact@maxence-lagalle.fr>
2022-03-29 16:39:04 +0200

    Linting

--- server/test_api.py
+++ server/test_api.py
@@ -1,16 +1,20 @@
 from server.api import get_ean, is_vegan
 
+
 def test_get_ean_Nutella():
     actual = get_ean("Nutella")
     assert actual == 3017620422003
+
 
 def test_get_ean_Camembert_President():
     actual = get_ean("Camembert Président")
     assert actual == 3228021170039
 
+
 def test_is_vegan_3468570116601():
     actual = is_vegan(3468570116601)
     assert actual == {3468570116601: "yes"}
+
 
 def test_is_vegan_3256540001305():
     actual = is_vegan(3256540001305)
