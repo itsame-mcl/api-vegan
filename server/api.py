e869f72e0437bdaf82f85db8b4e38d2d7c5bd540
Maxence Lagalle <contact@maxence-lagalle.fr>
2022-03-29 15:40:45 +0200

    EndPoint is vegan (match, case)

--- server/api.py
+++ server/api.py
@@ -13,12 +13,13 @@ def read_root():
 def is_vegan(item_id: int):
     result = OFF.get_info(item_id)
     ingredients_analysis_tags = result['product']['ingredients_analysis_tags']
-    if "en:vegan" in ingredients_analysis_tags:
-        is_vegan = "yes"
-    elif "en:non-vegan" in ingredients_analysis_tags:
-        is_vegan = "no"
-    elif "en:vegan-status-maybe" in ingredients_analysis_tags:
-        is_vegan = "maybe"
-    else:
-        is_vegan = "unknown"
+    match ingredients_analysis_tags:
+        case _ if "en:vegan" in ingredients_analysis_tags:
+            is_vegan = "yes"
+        case _ if "en:non-vegan" in ingredients_analysis_tags:
+            is_vegan = "no"
+        case _ if "en:vegan-status-maybe" in ingredients_analysis_tags:
+            is_vegan = "maybe"
+        case _:
+            is_vegan = "unknown"
     return {item_id: is_vegan}
